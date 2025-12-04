06ec127a2cde638214d623c99bf0dcfd30c1ed61
Akinwumi Akinfemi <kingfanak@gmail.com>
2025-12-04 12:11:15 +0100

    refactor HTML structure and enhance search bar functionality; update styles for improved visibility and loading state

--- script.js
+++ script.js
@@ -1,15 +1,68 @@
-console.log("Hello, World!");
-
-// handle dropdown toggle for units dropdown
+//Elements Selectors
+let currentUnit = "metric";
+// Units Dropdown
 const unitBtn = document.querySelector(".unit_button");
 const unitDropdown = document.querySelector(".units_dropdown");
+const dropdownOptions = document.querySelectorAll(".dropdown_option");
+const toggleSwitch = document.querySelector(".switch_units");
 
-unitBtn.addEventListener("click", () => {
+// Search Bar
+const search = document.getElementById("searchInput");
+const searchBtn = document.querySelector(".search_button");
+const suggestionBox = document.querySelector(".search_suggestion");
+
+// object to hold weather icon
+const weatherCodeToIcon = {
+  0: "icon-sunny",
+  1: "icon-partly-cloudy",
+  2: "icon-partly-cloudy",
+  3: "icon-overcast",
+  45: "icon-fog",
+  48: "icon-fog",
+  51: "icon-drizzle",
+  53: "icon-drizzle",
+  55: "icon-drizzle",
+  61: "icon-rain",
+  63: "icon-rain",
+  65: "icon-rain",
+  71: "icon-snow",
+  73: "icon-snow",
+  75: "icon-snow",
+  95: "icon-storm",
+  96: "icon-storm",
+  99: "icon-storm",
+};
+
+const getIconName = (code) => {
+  if (!(code in weatherCodeToIcon)) {
+    return "icon-sunny";
+  }
+  return weatherCodeToIcon[code];
+};
+
+//Functions
+function showLoadingState() {
+  suggestionBox.classList.add("visible");
+  searchBtn.disabled = true;
+  search.disabled = true;
+  searchBtn.style.cursor = "not-allowed";
+}
+
+function hideLoadingState() {
+  suggestionBox.classList.remove("visible");
+  searchBtn.disabled = false;
+  searchBtn.style.cursor = "pointer";
+  search.disabled = false;
+  console.log("loading state: hidden");
+}
+
+function toggleUnitDropdown(e) {
+  e.stopPropagation();
   console.log("Dropdown clicked");
   unitDropdown.classList.toggle("show");
-});
+}
 
-document.addEventListener("click", (e) => {
+function closeDropDownOnClickOutside(e) {
   if (
     unitDropdown.classList.contains("show") &&
     !unitBtn.contains(e.target) &&
@@ -18,53 +71,258 @@ document.addEventListener("click", (e) => {
     console.log("clicked outside the dropdown");
     unitDropdown.classList.remove("show");
   }
-});
+}
+
+function handleOptionSelection(e) {
+  const clickedButton = e.currentTarget;
+  const section = clickedButton.closest(".dropdown_section");
+  const currentActive = section.querySelector(".active_option");
+
+  currentActive.classList.remove("active_option");
+  currentActive.querySelector("span").remove();
+  clickedButton.classList.add("active_option");
+
+  const span = document.createElement("span");
+  const img = document.createElement("img");
+  img.src = "./assets/images/icon-checkmark.svg";
+  img.alt = "checkmark icon";
+
+  clickedButton.appendChild(span);
+  span.appendChild(img);
+}
+
+function toggleUnitSystem(e) {
+  e.stopPropagation();
+  if (toggleSwitch.textContent === "Switch to Imperial") {
+    toggleSwitch.textContent = "Switch to Metric";
+    currentUnit = "imperial";
+  } else {
+    toggleSwitch.textContent = "Switch to Imperial";
+    currentUnit = "metric";
+  }
+
+  console.log("Current Unit System:", currentUnit);
+}
+
+async function handleSearchClick() {
+  console.log(search.value);
+  if (!search.value.trim()) {
+    console.log("search input is empty");
+    return;
+  }
+
+  showLoadingState();
+
+  try {
+    const result = await fetchCityWeather(search.value);
+
+    hideLoadingState();
+
+    if (!result) {
+      console.error("City not found");
+      return;
+    }
+    console.log("this is handlesearchclick result", result);
+
+    updateWeatherUI(result);
+
+    console.log("City Weather Result:", result);
+  } catch (error) {
+    hideLoadingState();
+    console.error("Error fetching city weather:", error);
+  }
+}
+
+function showSearchSuggestions(e) {
+  suggestionBox.classList.add("visible");
+}
+
+//Geocoding API Call
+async function getCoordinates(cityName) {
+  try {
+    console.log("Fetching coordinates for:", cityName);
+
+    const response = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=1`
+    );
+
+    if (!response.ok) {
+      throw new Error("Geocoding Failed");
+    }
+
+    const data = await response.json();
+    console.log("Geocoding data:", data);
+
+    if (!data.results || data.results.length === 0) {
+      throw new Error("No results found");
+    }
+
+    const { latitude, longitude, name, country } = data.results[0];
+    console.log(
+      `Coordinates for ${cityName}: Lat ${latitude}, Lon ${longitude}`
+    );
+    return { latitude, longitude, name, country };
+  } catch (error) {
+    console.log("Geocoding error:", error);
+    return null;
+  }
+}
+
+//weather Api to get city weather data
+async function getWeatherData(lat, lon) {
+  try {
+    console.log("Getting weather for coordinates:", lat, lon);
+
+    const response = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=auto`
+    );
+
+    if (!response.ok) throw new Error("Weather data fetch failed");
+
+    const data = await response.json();
+    console.log("Weather data:", data);
+    return data;
+  } catch (error) {
+    console.error("Weather data error:", error);
+    return null;
+  }
+}
+
+async function fetchCityWeather(cityName) {
+  try {
+    console.log("Fetching weather for city:", cityName);
+
+    const coords = await getCoordinates(cityName);
+
+    if (!coords) {
+      throw new Error("city not found");
+    }
+
+    const weatherData = await getWeatherData(coords.latitude, coords.longitude);
+
+    if (!weatherData) {
+      throw new Error("Failed to get weather data");
+    }
+
+    return { cityInfo: coords, weather: weatherData };
+  } catch (error) {
+    console.error("Failed to fetch city weather", error);
+    return null;
+  }
+}
+//function to convert celsius to fahrenheit
+function convertTemperatureUnits(unit, value) {
+  switch (unit) {
+    case "metric":
+      return value;
+    case "imperial":
+      return (value * 1.8 + 32).toFixed(1);
+    default:
+      return value;
+  }
+}
+
+//function to convert km/h to mph
+function convertWindSpeedUnits(unit, value) {
+  switch (unit) {
+    case "metric":
+      return value;
+    case "imperial":
+      return (value / 1.609).toFixed(1);
+    default:
+      return value;
+  }
+}
+
+//function to convert mm to inches
+function convertPrecipitationUnits(unit, value) {
+  switch (unit) {
+    case "metric":
+      return (value / 25.4).toFixed(1);
+    case "imperial":
+      return value;
+    default:
+      return value;
+  }
+}
+
+//function to update UI
+function updateWeatherUI(weatherData) {
+  console.log("Updating UI with:", weatherData);
+  const { cityInfo: city, weather } = weatherData;
+
+  //weather details
+  const currentWeather = weather.current;
+  const dailyWeather = weather.daily;
+  const hoourlyWeather = weather.hourly;
+  const temp = Math.round(currentWeather.temperature_2m);
+  const feelsLike = Math.round(currentWeather.apparent_temperature);
+  const humidity = currentWeather.relative_humidity_2m;
+  const wind = Math.round(currentWeather.wind_speed_10m);
+  const precipitation = currentWeather.precipitation;
+  const weatherCode = currentWeather.weather_code;
+  const img = document.querySelector(".hero_icon");
+
+  const iconName = getIconName(weatherCode);
+  console.log("icon name:", iconName);
+  console.log("precip UI with:", precipitation);
+
+  // Update city name and date
+  const cityName = document.querySelector(".city");
+  const date = document.querySelector(".date");
+  const cityTemp = document.querySelector(".city_temp");
+  const cityWind = document.querySelector(".js_wind");
+  const cityHumidity = document.querySelector(".js_humidity");
+  const cityFeelsLike = document.querySelector(".feels_like");
+  const cityPrecip = document.querySelector(".js_precip");
+  img.src = `./assets/images/${iconName}.webp`;
+  img.alt = `weather icon representing ${iconName.replace("icon-", "").replace("-", " ")}`;
+
+  cityName.textContent = `${city.name}, ${city.country}`;
+  date.textContent = new Date().toLocaleDateString(undefined, {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  cityTemp.textContent = `${temp}°`;
+  cityTemp.setAttribute("data-temp", temp);
+  cityWind.textContent = `${wind} km/h`;
+  cityWind.setAttribute("data-wind", wind);
+  cityHumidity.textContent = `${humidity}%`;
+  cityHumidity.setAttribute("data-humid", humidity);
+  cityFeelsLike.textContent = `${feelsLike}°`;
+  cityFeelsLike.setAttribute("data-temp", feelsLike);
+  cityPrecip.textContent = `${precipitation} in`;
+  cityPrecip.setAttribute("data-precip", precipitation);
+
+  if (currentUnit === "imperial") {
+    cityTemp.textContent = `${convertTemperatureUnits("imperial", temp)}°`;
+    cityWind.textContent = `${convertWindSpeedUnits("imperial", wind)} mph`;
+    cityHumidity.textContent = `${humidity}%`;
+    cityFeelsLike.textContent = `${convertTemperatureUnits("imperial", feelsLike)}°`;
+    cityPrecip.textContent = `${convertPrecipitationUnits("imperial", precipitation)} in`;
+  }
+}
+
+// Event Listeners
+
+unitBtn.addEventListener("click", toggleUnitDropdown);
+
+document.addEventListener("click", closeDropDownOnClickOutside);
 
 // handle dropdown option selection
-const dropdownOptions = document.querySelectorAll(".dropdown_option");
 
 dropdownOptions.forEach((option) => {
-  option.addEventListener("click", (e) => {
-    const clickedButton = e.currentTarget;
-    const section = clickedButton.closest(".dropdown_section");
-    const currentActive = section.querySelector(".active_option");
-
-    currentActive.classList.remove("active_option");
-    currentActive.querySelector("span").remove();
-    clickedButton.classList.add("active_option");
-
-    const span = document.createElement("span");
-    const img = document.createElement("img");
-    img.src = "./assets/images/icon-checkmark.svg";
-    img.alt = "checkmark icon";
-
-    clickedButton.appendChild(span);
-    span.appendChild(img);
-  });
+  option.addEventListener("click", handleOptionSelection);
 });
-  
+
 // handle switch toggle between imperial and metic
 
-const toggleSwitch = document.querySelector('.switch_units');
-
-toggleSwitch.addEventListener('click', () => {
-  
-toggleSwitch.textContent= (toggleSwitch.textContent === "Switch to Imperial") ? "Switch to Metric" : "Switch to Imperial";
-
-});
+toggleSwitch.addEventListener("click", toggleUnitSystem);
 
 // Search bar functionality
-const search = document.getElementById('searchInput');
-const searchBtn = document.querySelector('.search_button')
-const suggestionBox = document.querySelector('.search_suggestion')
 
-search.addEventListener('input', () => {
-  suggestionBox.classList.add('visible');
- 
-})
+search.addEventListener("input", showSearchSuggestions);
 
-searchBtn.addEventListener('click', (e) => {
-  const query = search.value;
-
-  console.log(query);
-});
+searchBtn.addEventListener("click", handleSearchClick);
